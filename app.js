00556b7862344319e8c1b686f14a7caeb6e7bef0
Sainathreddy2002 <81603634+Sainathreddy2002@users.noreply.github.com>
2021-12-17 16:33:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
     host: "sql5.freesqldatabase.com",
     user: "sql5459626",
     password: "z5eZQnTI7k",
-    database: ' sql5459626',
+    database: 'sql5459626',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
